f5d2bc748478823b9fdba011ac275b9cc4355835
Xuanxiang Pan <pan286@gmail.com>
2013-11-19 10:04:03 +0800

    初始化项目

---
+++ BaiduPhoto/ZYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZYAppDelegate.h
+//  BaiduPhoto
+//
+//  Created by Xuanxiang Pan on 13-11-19.
+//  Copyright (c) 2013年 Ziyisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaiduPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BaiduPhoto
+//
+//  Created by Xuanxiang Pan on 13-11-19.
+//  Copyright (c) 2013年 Ziyisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZYAppDelegate class]));
+    }
+}
